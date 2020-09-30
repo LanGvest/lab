35f3c03139b28ad4539717e7f78725f5388932aa
LanGvest <61324477+LanGvest@users.noreply.github.com>
2020-09-30 09:56:05 +0300

    Update lab_7.c

--- С/lab_7.c
+++ С/lab_7.c
@@ -6,21 +6,24 @@ Enter string values so that you can enter text containing spaces.
 */
 
 struct computer {
-    char[20] name;
+    char name[20];
     float price;
 };
 
 int main() {
     int n;
-    printf("The number of elements: ");
+    printf("The number of computers: ");
     scanf("%d", &n);
-    struct computer list[n] = {};
+    struct computer list[n];
     for(int i = 0; i < n; i++) {
         printf("The name of computer %d: ", i+1);
         scanf("%s", &list[i].name);
         printf("Price: ");
         scanf("%f", &list[i].price);
     }
-
+    printf("\n");
+    for(int i = 0; i < n; i++) {
+        printf("%s, %.2f\n", list[i].name, list[i].price);
+    }
     return 0;
 }
