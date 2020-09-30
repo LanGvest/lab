7955d32a482ddb1f885d3f8d73d1e14131158422
LanGvest <61324477+LanGvest@users.noreply.github.com>
2020-09-30 09:59:38 +0300

    Create lab_6.c

---
+++ С/lab_6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*
+Perform the fifth task for cases when the memory for a two-dimensional array is allocated dynamically depending on the entered M, N, K
+and is correctly released at the end of the program.
+*/
+
+int main() {
+    return 0;
+}
