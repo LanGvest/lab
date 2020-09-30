95dddae793ad91258de35c723b84a66f43425af6
LanGvest <61324477+LanGvest@users.noreply.github.com>
2020-09-30 09:08:57 +0300

    Update lab_1.c

--- С/lab_1.c
+++ С/lab_1.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/*
+Implement a program that evaluates an integer expression: A<<=~B*--C (types: unsigned char, int, short int).
+The program should ask the user for the values of variables A, B, and C.
+Calculate the specified expression and display the values of the changed A, B, and C again.
+*/
+
 int main() {
     unsigned char a;
     int b;
